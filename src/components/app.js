d7b2624d4c7788c2306c1767fc3dd7639551da25
Literall <pnguyen@icgconsulting.com>
2017-11-06 01:03:34 -0700

    added base

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
+
 export default class App extends Component {
   render() {
     return (
-      <div>
-          ...
+      <div className="app">
+        <div className="input">
+        <input className="form-control" type="text" placeholder="Enter in an artist name" />
+        <button>Search</button>
+        </div>
+        <div className="profile">
+            <div>Artist name</div>
+            <div>Artist details</div>
+        </div>
+        <div className="gallery">
+          <div>gallery</div>
+        </div>
       </div>
+
     );
   }
 }
