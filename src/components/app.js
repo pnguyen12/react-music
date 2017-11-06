bb066fc467449ea96e1edb9c76291aa659da3210
Literall <pnguyen@icgconsulting.com>
2017-11-06 12:08:05 -0700

    added search methods

--- src/components/app.js
+++ src/components/app.js
@@ -2,16 +2,42 @@ import React, { Component } from 'react';
 
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      query: ''
+    }
+
+    this.search = this.search.bind(this);
+    this.changeQuery = this.changeQuery.bind(this);
+  }
+
+  changeQuery(event) {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  search() {
+    console.log(JSON.stringify(this.state))
+  }
+
+  checkKey(event) {
+    if (event.key == 'Enter') {
+      this.search();
+    }
+  }
   render() {
     return (
       <div className="app">
         <div className="input">
-        <input className="form-control" type="text" placeholder="Enter in an artist name" />
-        <button>.........</button>
+          <input onKeyPress={this.checkKey.bind(this)} onChange={this.changeQuery} className="form-control" type="text" placeholder="Enter in an artist name" />
+          <button onClick={this.search}>Search</button>
         </div>
         <div className="profile">
-            <div>Artist name</div>
-            <div>Artist details</div>
+          <div>Artist name</div>
+          <div>Artist details</div>
         </div>
         <div className="gallery">
           <div>gallery</div>
